f972c8ae26d933f906dfe674494be556b22ac8d1
Edgar56 <edgaras.chalilovas@gmail.com>
2017-11-23 22:03:59 +0200

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VerGil
+ * Date: 2017-08-24
+ * Time: 9:03 PM
+ */
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
+
+foreach ($db as $key => $value) {
+
+    define(strtoupper($key),$value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+/*if($connection) {
+    echo "We are connected";
+}*/
+
+
+?>

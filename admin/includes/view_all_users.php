5f64aaf10da0975b5ca9c20705021fd2eb9e7277
Edgar56 <edgaras.chalilovas@gmail.com>
2017-12-11 21:20:56 +0200

    Adding users option and delete users

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -67,7 +67,7 @@
 
         echo "<td><a href='comments.php?approve='>Approve</a></td>";
         echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>";
-        echo "<td><a href='comments.php?delete='>Delete</a></td>";
+        echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
         echo "</tr>";
 
     }
@@ -100,12 +100,12 @@ if(isset($_GET['unapprove'])) {
 
 
 if(isset($_GET['delete'])) {
-    $the_comment_id = $_GET['delete'];
+    $the_user_id = $_GET['delete'];
 
-    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
-    $delete_query = mysqli_query($connection, $query);
+    $query = "DELETE FROM users WHERE user_id = {$the_user_id}";
+    $delete_user_query = mysqli_query($connection, $query);
 
-    header("Location: comments.php");
+    header("Location: users.php");
 }
 /*
 test

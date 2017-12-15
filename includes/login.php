f7626da8993f3b2f9d44fd305f6c79b2ed365f3c
Edgar56 <edgaras.chalilovas@gmail.com>
2017-12-15 18:12:13 +0200

    Creating login form

---
+++ includes/login.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VerGil-PC
+ * Date: 15/12/2017
+ * Time: 18:02
+ */
+include "db.php";
+?>
+
+<?php
+
+if(isset($_POST['login'])) {
+
+   echo $username = $_POST['username'];
+   echo $password = $_POST['password'];
+
+
+   
+
+
+}
+
+
+
+?>
+
+
+

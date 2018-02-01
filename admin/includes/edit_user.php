9bd0b8a54160312040b1ae73b867408188300719
Edgar56 <edgaras.chalilovas@gmail.com>
2018-02-01 22:14:35 +0200

    Ended section 26 about Password Encrypting and login system, user simple password encryption with function password('our password',Password_BCRYPT, array)

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,69 +1,71 @@
 <?php
 
-if(isset($_GET['edit_user'])) {
+if (isset($_GET['edit_user'])) {
     $the_user_id = $_GET['edit_user'];
 
-$query = "SELECT * FROM users WHERE user_id= $the_user_id ";
-$select_users_query = mysqli_query($connection,$query);
+    $query = "SELECT * FROM users WHERE user_id= $the_user_id ";
+    $select_users_query = mysqli_query($connection, $query);
 
-while($row = mysqli_fetch_assoc($select_users_query)) {
-    $user_id = $row['user_id'];
-    $username = $row['username'];
-    $user_password = $row['user_password'];
-    $user_firstname = $row['user_firstname'];
-    $user_lastname = $row['user_lastname'];
-    $user_email = $row['user_email'];
-    $user_image = $row['user_image'];
-    $user_role = $row['user_role'];
-}
+    while ($row = mysqli_fetch_assoc($select_users_query)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+    }
+    ?>
 
+    <?php
 
+    if (isset($_POST['edit_user'])) {
 
-}
-
-
-if(isset($_POST['edit_user'])) {
-
-    $user_firstname= $_POST['user_firstname'];
-    $user_lastname= $_POST['user_lastname'];
-    $user_role= $_POST['user_role'];
-
-//        $post_image= $_FILES['image']['name'];
-//        $post_image_temp= $_FILES['image']['tmp_name'];
-
-    $username= $_POST['username'];
-    $user_email= $_POST['user_email'];
-    $user_password= $_POST['user_password'];
-    //$post_date= date('d-m-y');
-
-    // move_uploaded_file($post_image_temp,"../images/$post_image");
+        $user_firstname = $_POST['user_firstname'];
+        $user_lastname = $_POST['user_lastname'];
+        $user_role = $_POST['user_role'];
+        $username = $_POST['username'];
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        $post_date = date('d-m-y');
 
 
-    $query = "SELECT randSalt FROM users";
-    $select_randsalt_query = mysqli_query($connection,$query);
-    if(!$select_randsalt_query) {
-        die("Query Failed" . mysqli_error($connection));
+        if (!empty($user_password)) {
+
+            $query_password = "SELECT user_password FROM users WHERE user_id = $the_user_id";
+            $get_user_query = mysqli_query($connection, $query_password);
+            confirmQuery($get_user_query);
+
+            $row = mysqli_fetch_array($get_user_query);
+
+            $db_user_password = $row['user_password'];
+        }
+
+        if ($db_user_password != $user_password) {
+            $hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
+        }
+
+        $query = "UPDATE users SET ";
+        $query .= "user_firstname = '{$user_firstname}', ";
+        $query .= "user_lastname = '{$user_lastname}', ";
+        $query .= "user_role = '{$user_role}', ";
+        $query .= "username = '{$username}', ";
+        $query .= "user_email = '{$user_email}', ";
+        $query .= "user_password = '{$hashed_password}' ";
+        $query .= "WHERE user_id = {$the_user_id} ";
+
+        $edit_user_query = mysqli_query($connection, $query);
+        confirmQuery($edit_user_query);
+        echo "User Updated" . " <a href='users.php'>View Users?</a>";
     }
 
-    $row = mysqli_fetch_array($select_randsalt_query);
-    $salt = $row['randSalt'];
-    $hashed_password = crypt($user_password, $salt);
+} else {
+
+    header("Location: index.php ");
 
 
-    $query = "UPDATE users SET ";
-    $query.="user_firstname='{$user_firstname}', ";
-    $query.="user_lastname='{$user_lastname}', ";
-    $query.="user_role= '{$user_role}',";
-    $query.="username='{$username}', ";
-    $query.="user_email='{$user_email}', ";
-    $query.="user_password='{$hashed_password}' ";
-    $query.="WHERE user_id = {$the_user_id} ";
-
-    $edit_user_query= mysqli_query($connection,$query);
-
-    confirmQuery($edit_user_query);
 }
-
 
 ?>
 
@@ -80,7 +82,6 @@ if(isset($_POST['edit_user'])) {
     </div>
 
 
-
     <div class="form-group">
 
         <select name="user_role" id="">
@@ -88,20 +89,12 @@ if(isset($_POST['edit_user'])) {
 
             <?php
 
-            if($user_role=='admin') {
-              echo  "<option value='subscriber'>subscriber</option>";
+            if ($user_role == 'admin') {
+                echo "<option value='subscriber'>subscriber</option>";
             } else {
-              echo "<option value='admin'>admin</option>" ;
+                echo "<option value='admin'>admin</option>";
             }
-
-
             ?>
-
-
-
-
-
-
         </select>
 
     </div>
--- registration.php
+++ registration.php
@@ -16,16 +16,17 @@ if (isset($_POST['submit'])) {
         $password = mysqli_real_escape_string($connection, $password);
 
 
-        $query = "SELECT randSalt FROM users";
-        $select_randSalt_query = mysqli_query($connection, $query);
+        $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
 
-        if (!$select_randSalt_query) {
-            die("Query failed" . mysqli_error($connection));
-        }
 
-        $row = mysqli_fetch_array($select_randSalt_query);
-        $salt = $row['randSalt'];
-        $password = crypt($password, $salt);
+//        $query = "SELECT randSalt FROM users";
+//        $select_randSalt_query = mysqli_query($connection, $query);
+//        if (!$select_randSalt_query) {
+//            die("Query failed" . mysqli_error($connection));
+//        }
+//        $row = mysqli_fetch_array($select_randSalt_query);
+//        $salt = $row['randSalt'];
+//        $password = crypt($password, $salt);
 
 
         $query = "INSERT INTO users (username, user_email, user_password,user_role) ";

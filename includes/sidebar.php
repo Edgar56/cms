9a3ea01db116a42631b9d28a4616904caa76d079
Edgar56 <edgaras.chalilovas@gmail.com>
2018-02-15 22:25:38 +0200

    Ended sections 34 Some miscellaneous features

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,3 @@
-
-
 <div class="col-md-4">
 
     <!-- Blog Search Well -->
@@ -19,26 +17,36 @@
     </div>
 
 
-
     <!-- Login -->
     <div class="well">
-        <h4>Login</h4>
-        <form action="includes/login.php" method="post">
-            <div class="form-group">
-                <input name="username" type="text" class="form-control" placeholder="Enter Username">
-            </div>
 
-            <div class="input-group">
-                <input name="password" type="password" class="form-control" placeholder="Enter Password">
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
-                </span>
-            </div>
-        </form><!--search form-->
-        <!-- /.input-group -->
+        <?php if (isset($_SESSION['user_role'])): ?>
+            <h4>Logged in as <?php echo $_SESSION['username'] ?> </h4>
+
+            <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+
+        <?php else: ?>
+
+            <h4>Login</h4>
+
+            <form action="includes/login.php" method="post">
+                <div class="form-group">
+                    <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                </div>
+
+                <div class="input-group">
+                    <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                 </span>
+                </div>
+            </form><!--search form-->
+            <!-- /.input-group -->
+
+        <?php endif; ?>
+
+
     </div>
-
-
 
 
     <!-- Blog Categories Well -->
@@ -55,19 +63,18 @@
         ?>
 
 
-
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-12">
                 <ul class="list-unstyled">
-        <?php
+                    <?php
                     while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-                    $cat_title = $row['cat_title'];
-                    $cat_id = $row['cat_id'];
-                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+                        $cat_title = $row['cat_title'];
+                        $cat_id = $row['cat_id'];
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
 
-        ?>
+                    ?>
                 </ul>
             </div>
 
